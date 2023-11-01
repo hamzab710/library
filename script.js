107942e8d0d5cd2f1517cc2c72df2a3c978b9fb9
hamzab710 <hamzabessali8@gmail.com>
2023-11-01 12:39:01 +0100

    Add edit function

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-myLibrary = [];
-
-function Book(name, author, pages, readingStatus) {
-    this.name = name,
-    this.author = author,
-    this.pages = pages,
-    this.readingStatus = readingStatus
-}
-
 const form = document.querySelector('form');
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    addBookToLibrary();
-    createBook();
-    dialog.close();
-});
-
 const anewBook = document.getElementById('new-book');
 const dialog = document.querySelector('dialog');
+const legend = document.querySelector('legend');
+const bookLibrary = document.getElementById('book-library');
+
 anewBook.addEventListener('click', () => {
+    legend.textContent = 'New Book';
     dialog.showModal();
 });
 
@@ -27,121 +15,153 @@ document.addEventListener('click', function (event) {
     }
 });
 
-//funtion to push the new added book to myLibrary array
-function addBookToLibrary() {
-    const nameInput = document.getElementById('name');
-    const authorInput = document.getElementById('author');
-    const pagesInput = document.getElementById('pages');
-    const readingStatusInput = document.querySelector('input[name="reading-status"]:checked');
+myLibrary = [];
+
+function Book(name, author, pages, readingStatus) {
+    this.name = name,
+    this.author = author,
+    this.pages = pages,
+    this.readingStatus = readingStatus,
+    this.id = Math.floor(Math.random() * 1000000);
+}
+
+form.addEventListener('submit', function (event) {
+    if (legend.textContent == 'New Book') {
+    event.preventDefault();
+    let nameInput = document.getElementById('name');
+    let authorInput = document.getElementById('author');
+    let pagesInput = document.getElementById('pages');
+    let readingStatusInput = document.querySelector('input[name="reading-status"]:checked');
+
+    const name = nameInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const readingStatus = readingStatusInput.value;
     
-    const book = new Book(nameInput.value, authorInput.value, pagesInput.value, readingStatusInput.value);
-    myLibrary.push(book);
-}
+    let newBook = new Book(name, author, pages, readingStatus)
+    let newBookContainer = new BookContainer(newBook);
+    myLibrary.push(newBook);
+    bookLibrary.appendChild(newBookContainer.element);
+    dialog.close();
 
-// funtion that hides the book information when clicking on the book name
-function hideDetails(bookAuthor, bookPages, bookReadingStatus) {
-    bookAuthor.style.display = 'none';
-    bookPages.style.display = 'none';
-    bookReadingStatus.style.display = 'none';
-}
-
-// funtion that shows the book information when clicking on the book name
-function showDetails(bookAuthor, bookPages, bookReadingStatus) {
-    bookAuthor.style.display = 'block';
-    bookPages.style.display = 'block';
-    bookReadingStatus.style.display = 'block';
-}
-
-// funtion that changes the color depending on if the book has been read or not
-function changeReadingStatus(readingStatusInput, bookReadingStatus) {
-    if (readingStatusInput.value == 'read') {
-        readingStatusInput.value = 'not read';
-        bookReadingStatus.textContent = readingStatusInput.value;
-        bookReadingStatus.style.color = 'red';
-    } else {
-        readingStatusInput.value = 'read';
-        bookReadingStatus.textContent = readingStatusInput.value;
-        bookReadingStatus.style.color = 'green';
-    }
-}
-
-// funtion that deletes a book
-function removeBook(container) {
-    const bookName = container.querySelector('#book-name').textContent;
-    const index = myLibrary.findIndex(book => book.name === bookName);
-
-    if (index !== -1) {
-        myLibrary.splice(index, 1);
-    }
-
-    container.parentNode.removeChild(container);
-}
-
-
-// function to create element that display the book information
-function createBook() {
-    const myBooks = document.getElementById('book-library');
-    myBooks.innerHTML = '';
-
-    const nameInput = document.getElementById('name');
-    const authorInput = document.getElementById('author');
-    const pagesInput = document.getElementById('pages');
-    const readingStatusInput = document.querySelector('input[name="reading-status"]:checked');
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        const bookInfo = myLibrary[i];
-
-        const container = document.createElement('div');
-        container.id = 'container';
-        myBooks.appendChild(container);
-
-        const bookName = document.createElement('button');
-        const bookAuthor = document.createElement('p');
-        const bookPages = document.createElement('p');
-        const bookReadingStatus = document.createElement('button');
-        
-        bookName.textContent = bookInfo.name;
-        bookAuthor.textContent = bookInfo.author;
-        bookPages.textContent = `${bookInfo.pages} pages`;
-        bookReadingStatus.textContent = bookInfo.readingStatus;
-
-        bookReadingStatus.id = 'reading-status';
-        if (bookInfo.readingStatus == 'read') {
-            bookReadingStatus.style.color = 'green';
-        } else {
-            bookReadingStatus.style.color = 'red';
-        }
-
-        container.appendChild(bookName);
-        container.appendChild(bookAuthor);
-        container.appendChild(bookPages);
-        container.appendChild(bookReadingStatus);
-
-        const bookRemover = document.createElement('img');
-        bookRemover.id = 'book-remover';
-        bookRemover.src = 'icons/X.svg';
-        container.appendChild(bookRemover);
-
-        bookRemover.addEventListener('click', () => {
-            removeBook(container)
-        });
-    
-        bookName.id = 'book-name';
-    
-        bookName.addEventListener('click', () => {
-            if (bookAuthor.style.display == 'none' && bookPages.style.display == 'none' && bookReadingStatus.style.display == 'none') {
-                showDetails(bookAuthor, bookPages, bookReadingStatus)
-            } else {
-                hideDetails(bookAuthor, bookPages, bookReadingStatus)
-            }
-        })
-
-        bookReadingStatus.addEventListener('click', () => {
-            changeReadingStatus(readingStatusInput, bookReadingStatus)
-        });
-    }
     nameInput.value = '';
     authorInput.value = '';
     pagesInput.value = '';
-    readingStatusInput.checked = false;
+    if (readingStatusInput) {
+        readingStatusInput.checked = false;
+    }
+    }
+});
+
+// function to create element that display the book information
+function BookContainer(book) {
+    this.element = document.createElement('div');
+    this.element.id = 'container';
+
+    this.element.dataset.id = book.id;
+
+    this.name = document.createElement('button');
+    this.name.classList.add('name');
+    this.name.textContent = book.name;
+    this.element.appendChild(this.name);
+
+    this.author = document.createElement('p');
+    this.author.classList.add('author');
+    this.author.textContent = book.author;
+    this.element.appendChild(this.author);
+
+    this.pages = document.createElement('p');
+    this.pages.classList.add('pages');
+    this.pages.textContent = book.pages;
+    this.element.appendChild(this.pages);
+
+    this.read = document.createElement('button');
+    this.read.classList.add('read');
+    this.read.textContent = book.readingStatus;
+    this.element.appendChild(this.read);
+
+    if (book.readingStatus == 'read') {
+        this.read.style.color = 'green';
+    } else {
+        this.read.style.color = 'red';
+    }
+
+    this.read.addEventListener('click', () => {
+        if (book.readingStatus == 'read') {
+            book.readingStatus = 'not read';
+            this.read.textContent = book.readingStatus;
+            this.read.style.color = 'red';
+        } else {
+            book.readingStatus = 'read';
+            this.read.textContent = book.readingStatus;
+            this.read.style.color = 'green';
+        }
+    });
+
+    this.editButton = document.createElement('img');
+    this.editButton.classList.add('edit-icon');
+    this.editButton.src = 'icons/pencil.svg';
+    this.element.appendChild(this.editButton);
+
+    let currentlyEditedBook = null;
+
+    this.editButton.addEventListener('click', () => {
+        currentlyEditedBook = book;
+        legend.textContent = 'Edit Book';
+        dialog.showModal();
+        form.addEventListener('submit', (e) => {
+            if (currentlyEditedBook) {
+                e.preventDefault();
+                let nameInput = document.getElementById('name');
+                let authorInput = document.getElementById('author');
+                let pagesInput = document.getElementById('pages');
+                let readingStatusInput = document.querySelector('input[name="reading-status"]:checked');
+
+                const name = nameInput.value;
+                const author = authorInput.value;
+                const pages = pagesInput.value;
+                const readingStatus = readingStatusInput.value;
+
+                currentlyEditedBook.name = name;
+                currentlyEditedBook.author = author;
+                currentlyEditedBook.pages = pages;
+                currentlyEditedBook.readingStatus = readingStatus;
+    
+                updateBookContainer(currentlyEditedBook);
+                dialog.close();
+                currentlyEditedBook = null;
+
+                nameInput.value = '';
+                authorInput.value = '';
+                pagesInput.value = '';
+                if (readingStatusInput) {
+                    readingStatusInput.checked = false;
+                }
+            }
+        })
+    })
+
+    this.bookRemover = document.createElement('img');
+    this.bookRemover.classList.add('remove-icon');
+    this.bookRemover.src = 'icons/X.svg';
+    this.element.appendChild(this.bookRemover);
+
+    this.bookRemover.addEventListener('click', () => {
+        let index = myLibrary.findIndex((b) => b.id === book.id);
+        myLibrary.splice(index, 1);
+    
+        bookLibrary.removeChild(this.element);
+    })
 }
+
+function updateBookContainer(book) {
+    const bookContainer = document.querySelector(`div[data-id="${book.id}"]`);
+    if (bookContainer) {
+        bookContainer.querySelector('.name').textContent = book.name;
+        bookContainer.querySelector('.author').textContent = book.author;
+        bookContainer.querySelector('.pages').textContent = book.pages;
+        bookContainer.querySelector('.read').textContent = book.readingStatus;
+        bookContainer.querySelector('.read').style.color = book.readingStatus === 'read' ? 'green' : 'red';
+    }
+}
+
